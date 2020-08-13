5ab871a8c5f57714106a4f0521984b3fbd82b5af
Erick Cuevas <erickcufe94@gmail.com>
2020-08-13 12:12:12 -0500

    check color based on cowsay

---
+++ R/check_color.R
@@ -0,0 +1,20 @@
+check_color <- function(clr) {
+  if (!is.null(clr) && !all(inherits(clr, c("crayon", "character")))) {
+    stop("All colors must be of class character or crayon",
+         call. = FALSE)
+  }
+
+  if (all(inherits(clr, "character"))) {
+    if (length(clr) > 1 || clr == "rainbow") {
+      check4pkg("multicolor")
+
+      clr <- multicolor::insert_rainbow(clr)
+
+      if (!all(is.character(clr))) {
+        stop("If color arguments have > length 1, all colors must be of class character",
+             call. = FALSE)
+      }
+    }
+  }
+  return(clr)
+}
